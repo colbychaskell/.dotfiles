2fca9fb03b1cdb0e1c406f593aa508981155b267
Colby Haskell <chaskell35@gmail.com>
2024-08-11 20:44:25 -0400

    Add relative line numbers in netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Enable relative linenumbers in netrw
+vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
