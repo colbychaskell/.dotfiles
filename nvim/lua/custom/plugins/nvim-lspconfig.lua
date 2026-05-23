f63c0a88fd1b5dcff0a8340c01603ed3fc4c5f38
Colby Haskell <colbyhas@amazon.com>
2026-05-23 20:15:55 +0000

    Stop using mason-tool-installer for nix-managed LSP tools

--- nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -197,21 +197,9 @@ return {
       },
     }
 
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+    -- LSP servers and formatters are installed via nix (home-manager).
+    -- Mason is kept for anything not yet packaged in nixpkgs.
+    require("mason-tool-installer").setup({ ensure_installed = {} })
 
     require("mason-lspconfig").setup({
       ensure_installed = {},
