453fb59522499e036d45fa01449202dc8d7f690c
Colby Haskell <chaskell35@gmail.com>
2026-05-24 11:23:54 -0400

    Switch SSH config to declarative programs.ssh module
    Use programs.ssh with settings API instead of raw config file.
    Disables default config and explicitly declares all settings for
    future compatibility.

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -1,5 +1,16 @@
 { ... }:
 
 {
-  home.file.".ssh/config".source = ./config;
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    includes = [ "config.d/*" ];
+    settings = {
+      "*" = {
+        AddKeysToAgent = "yes";
+        ServerAliveInterval = 29;
+        IdentityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
 }
