b209aff38ea4ace8642b425b3b0c013d8ac32bc6
Colby Haskell <chaskell35@gmail.com>
2026-05-27 08:06:04 -0400

    Update treesitter for nvim 0.12

--- home/neovim/nvim/lua/custom/plugins/treesitter.lua
+++ home/neovim/nvim/lua/custom/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
+  config = function()
+    require("nvim-treesitter").setup()
+
+    require("nvim-treesitter").install({
       "bash",
       "c",
       "cpp",
@@ -12,37 +14,20 @@ return { -- Highlight, edit, and navigate code
       "lua",
       "luadoc",
       "markdown",
+      "markdown_inline",
       "vim",
       "vimdoc",
       "gitignore",
       "gitcommit",
       "git_config",
       "git_rebase",
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby", "lua" },
-    },
-    -- indent = { enable = true, disable = { "ruby" } },
-  },
-  config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    })
 
-    -- Prefer git instead of curl in order to improve connectivity in some environments
-    require("nvim-treesitter.install").prefer_git = true
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup(opts)
-
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+      end,
+    })
   end,
 }
